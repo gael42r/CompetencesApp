c46a31662ba675f0f2f45ac0e2084c29a2a7e50b
Gael louis Riou <gael.louis.riou@etu.univ-st-etienne.fr>
2020-06-12 16:40:25 +0200

    Forms Add

---
+++ CompetencesApp/FormAddUser.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace CompetencesApp
+{
+    partial class FormAddUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAddUser));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelPrenom = new System.Windows.Forms.Label();
+            this.labelNom = new System.Windows.Forms.Label();
+            this.textBoxPrenom = new System.Windows.Forms.TextBox();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelStatut = new System.Windows.Forms.Label();
+            this.comboBoxStatut = new System.Windows.Forms.ComboBox();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel2.Controls.Add(this.buttonClose);
+            this.panel2.Controls.Add(this.labelTitle);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(676, 73);
+            this.panel2.TabIndex = 20;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackColor = System.Drawing.Color.Transparent;
+            this.buttonClose.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonClose.BackgroundImage")));
+            this.buttonClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Location = new System.Drawing.Point(639, 12);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(25, 25);
+            this.buttonClose.TabIndex = 8;
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.labelTitle.Location = new System.Drawing.Point(20, 12);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(339, 45);
+            this.labelTitle.TabIndex = 27;
+            this.labelTitle.Text = "Ajouter un utilisateur";
+            // 
+            // labelPrenom
+            // 
+            this.labelPrenom.AutoSize = true;
+            this.labelPrenom.BackColor = System.Drawing.Color.Transparent;
+            this.labelPrenom.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrenom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.labelPrenom.Location = new System.Drawing.Point(23, 77);
+            this.labelPrenom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelPrenom.Name = "labelPrenom";
+            this.labelPrenom.Size = new System.Drawing.Size(102, 30);
+            this.labelPrenom.TabIndex = 28;
+            this.labelPrenom.Text = "Prénom :";
+            // 
+            // labelNom
+            // 
+            this.labelNom.AutoSize = true;
+            this.labelNom.BackColor = System.Drawing.Color.Transparent;
+            this.labelNom.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.labelNom.Location = new System.Drawing.Point(23, 145);
+            this.labelNom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelNom.Name = "labelNom";
+            this.labelNom.Size = new System.Drawing.Size(74, 30);
+            this.labelNom.TabIndex = 29;
+            this.labelNom.Text = "Nom :";
+            // 
+            // textBoxPrenom
+            // 
+            this.textBoxPrenom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBoxPrenom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.textBoxPrenom.Location = new System.Drawing.Point(28, 111);
+            this.textBoxPrenom.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.textBoxPrenom.Name = "textBoxPrenom";
+            this.textBoxPrenom.Size = new System.Drawing.Size(260, 30);
+            this.textBoxPrenom.TabIndex = 30;
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBoxNom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.textBoxNom.Location = new System.Drawing.Point(28, 179);
+            this.textBoxNom.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(260, 30);
+            this.textBoxNom.TabIndex = 31;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.BackColor = System.Drawing.Color.Transparent;
+            this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.labelUsername.Location = new System.Drawing.Point(383, 77);
+            this.labelUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(198, 30);
+            this.labelUsername.TabIndex = 32;
+            this.labelUsername.Text = "Nom d\'utilisateur :";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBoxUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.textBoxUsername.Location = new System.Drawing.Point(388, 111);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(260, 30);
+            this.textBoxUsername.TabIndex = 33;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.BackColor = System.Drawing.Color.Transparent;
+            this.labelPassword.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.labelPassword.Location = new System.Drawing.Point(383, 145);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(155, 30);
+            this.labelPassword.TabIndex = 34;
+            this.labelPassword.Text = "Mot de passe :";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBoxPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.textBoxPassword.Location = new System.Drawing.Point(388, 179);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(260, 30);
+            this.textBoxPassword.TabIndex = 35;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAdd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonAdd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.buttonAdd.Location = new System.Drawing.Point(28, 300);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(620, 40);
+            this.buttonAdd.TabIndex = 38;
+            this.buttonAdd.Text = "Ajouter";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // labelStatut
+            // 
+            this.labelStatut.AutoSize = true;
+            this.labelStatut.BackColor = System.Drawing.Color.Transparent;
+            this.labelStatut.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.labelStatut.Location = new System.Drawing.Point(23, 213);
+            this.labelStatut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelStatut.Name = "labelStatut";
+            this.labelStatut.Size = new System.Drawing.Size(84, 30);
+            this.labelStatut.TabIndex = 39;
+            this.labelStatut.Text = "Statut :";
+            // 
+            // comboBoxStatut
+            // 
+            this.comboBoxStatut.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxStatut.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.comboBoxStatut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.comboBoxStatut.FormattingEnabled = true;
+            this.comboBoxStatut.Location = new System.Drawing.Point(28, 246);
+            this.comboBoxStatut.Name = "comboBoxStatut";
+            this.comboBoxStatut.Size = new System.Drawing.Size(260, 38);
+            this.comboBoxStatut.TabIndex = 40;
+            // 
+            // FormAddUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(676, 355);
+            this.Controls.Add(this.comboBoxStatut);
+            this.Controls.Add(this.labelStatut);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxNom);
+            this.Controls.Add(this.textBoxPrenom);
+            this.Controls.Add(this.labelNom);
+            this.Controls.Add(this.labelPrenom);
+            this.Controls.Add(this.panel2);
+            this.Name = "FormAddUser";
+            this.Text = "FormAddUser";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelPrenom;
+        private System.Windows.Forms.Label labelNom;
+        private System.Windows.Forms.TextBox textBoxPrenom;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelStatut;
+        private System.Windows.Forms.ComboBox comboBoxStatut;
+    }
+}
---
+++ CompetencesApp/FormAddUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompetencesApp
+{
+    public partial class FormAddUser : Form2
+    {
+        public FormAddUser()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
